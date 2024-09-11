97e0417320b0d87acd04666fc6cff81e071a443d
Jeff <jeff@heat.tech>
2024-09-11 13:02:27 -0700

    jeffs python scripts

--- datasets/heat/blender_scripts/voxelize_vrt_files.py
+++ datasets/heat/blender_scripts/voxelize_vrt_files.py
@@ -5,7 +5,7 @@ import subprocess
 def run_voxelizer(directory):
     # Loop through all files in the given directory
     for filename in os.listdir(directory):
-        if filename.endswith(".vrt_body.bin"):
+        if filename.endswith(".vrt_hand.bin"):
             abs_path = os.path.join(directory, filename)
             base_name = os.path.splitext(os.path.basename(abs_path))[0]
             output_path = os.path.join(directory, f"{base_name}.vox.bin")
@@ -24,7 +24,16 @@ def run_voxelizer(directory):
 if __name__ == "__main__":
     # Replace these with your directory paths
     directory_paths = [
-        'F:\\heat_primer_dataset\\processed',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand1',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand2',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand3',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand4',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand5',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand6',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand7',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand8',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand9',
+        'Z:\\Jeffs corner\\hand datasets\\hands\\2f_rd3\\hand10',
         # Add more paths as needed
     ]
 
